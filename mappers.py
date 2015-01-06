be5151d57053bb54ca460b8004a0f529178fe3bc
Tarmo Toikkanen <tarmo@iki.fi>
2015-01-06 16:05:36 +0200

    Added example mapper definition.

---
+++ mappers.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+emappers = {
+        u'Example 1': [u'partial pattern 1',u'partial pattern 2'],
+        u'Example 2': [u'partial pattern 3'],
+        }
